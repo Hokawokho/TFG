7be9cb9d6cdc92419a46b3a44306b963f1c0cb6d
Hokawokho <jordipeiropuig@gmail.com>
2025-04-02 16:44:23 +0200

    asdf

--- Assets/Scipts/SuperiorRotation.cs
+++ Assets/Scipts/SuperiorRotation.cs
@@ -45,34 +45,45 @@ public class SuperiorRotation : MonoBehaviour
 
     public void RotateTopMap()
     {
-        Dictionary<Transform, Vector2Int> previousPositions = new Dictionary<Transform, Vector2Int>();
-
-        // 1. Guardar las posiciones actuales de las unidades antes de rotar
-        foreach (Transform unit in transform.GetComponentsInChildren<Transform>())
-        {
-            if (unit.CompareTag("Unit"))
-            {
-                int posX = Mathf.RoundToInt(unit.position.x) / gridManager.UnityGridSize;
-                int posY = Mathf.RoundToInt(unit.position.z) / gridManager.UnityGridSize;
-                Vector2Int unitPosition = new Vector2Int(posX, posY);
-
-                previousPositions[unit] = unitPosition;
-            }
-        }
-
-        // 2. Aplicar la rotación
         currentRotation += 90f;
         if (currentRotation >= 360f)
         {
             currentRotation = 0f; // Reiniciar la rotación
         }
         transform.rotation = Quaternion.Euler(0, currentRotation, 0);
-
-        // 3. Desbloquear las casillas donde estaban antes de rotar
-        foreach (var entry in previousPositions)
-        {
-            gridManager.UnblockNode(entry.Value);
-        }
         
     }
+
+    // public void RotateTopMap()
+    // {
+    //     Dictionary<Transform, Vector2Int> previousPositions = new Dictionary<Transform, Vector2Int>();
+
+    //     // 1. Guardar las posiciones actuales de las unidades antes de rotar
+    //     foreach (Transform unit in transform.GetComponentsInChildren<Transform>())
+    //     {
+    //         if (unit.CompareTag("Unit"))
+    //         {
+    //             int posX = Mathf.RoundToInt(unit.position.x) / gridManager.UnityGridSize;
+    //             int posY = Mathf.RoundToInt(unit.position.z) / gridManager.UnityGridSize;
+    //             Vector2Int unitPosition = new Vector2Int(posX, posY);
+
+    //             previousPositions[unit] = unitPosition;
+    //         }
+    //     }
+
+    //     // 2. Aplicar la rotación
+    //     currentRotation += 90f;
+    //     if (currentRotation >= 360f)
+    //     {
+    //         currentRotation = 0f; // Reiniciar la rotación
+    //     }
+    //     transform.rotation = Quaternion.Euler(0, currentRotation, 0);
+
+    //     //3. Desbloquear las casillas donde estaban antes de rotar
+    //     foreach (var entry in previousPositions)
+    //     {
+    //         gridManager.UnblockNode(entry.Value);
+    //     }
+        
+    // }
 }
